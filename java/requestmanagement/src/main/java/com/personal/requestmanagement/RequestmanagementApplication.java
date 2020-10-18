42e29cd9317a8e0edaedb9e67eea528734cd51f2
quyetnv98 <quyetnv62@wru.vn>
2020-10-18 21:47:48 +0700

    commit bai tap lon

---
+++ java/requestmanagement/src/main/java/com/personal/requestmanagement/RequestmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.personal.requestmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestmanagementApplication.class, args);
+	}
+
+}
---
+++ java/requestmanagement/src/test/java/com/personal/requestmanagement/RequestmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.requestmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
