0b663c98ab6c8aadf6046c644eb1ee4b282d7afe
quyetnv98 <quyetnv62@wru.vn>
2020-10-19 21:04:00 +0700

    pom + model

---
+++ java/requestmanagement/src/main/java/com/personal/requestmanagement/model/entity/Role.java
@@ -0,0 +1,44 @@
+package com.personal.requestmanagement.model.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "role")
+public class Role implements Serializable, GrantedAuthority {
+
+	@Transient
+	private static final long serialVersionUID = -7402837321796905823L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "role_code")
+	private String roleCode;
+	
+	@Column(name = "role_name")
+	private String roleName;
+
+	@Override
+	@Transient
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return "ROLE_" + this.roleCode;
+	}
+
+}
---
+++ java/requestmanagement/src/main/java/com/personal/requestmanagement/model/entity/User.java
@@ -0,0 +1,75 @@
+package com.personal.requestmanagement.model.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User implements Serializable, UserDetails {
+
+	@Transient
+	private static final long serialVersionUID = -769998853114566733L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "user_name")
+	private String userName;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
