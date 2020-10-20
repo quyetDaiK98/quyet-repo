9695aac3d31d14af7f22da1584caefb7a7d7e405
QUYET Nguyen Van (VTI.D1) <quyet.nguyenvan@vti.vn>
2020-10-20 17:32:07 +0700

    make login part 2

--- java/requestmanagement/src/main/java/com/personal/requestmanagement/controller/HomeController.java
+++ java/requestmanagement/src/main/java/com/personal/requestmanagement/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/index")
 	public String home(Model model) {
 		model.addAttribute("test", "quyetDaiK");
 		return "index";
